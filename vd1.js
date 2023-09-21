f7718ecc256e0dd65791b228fc4901aaa19f4750
TinhTran <riotgheptrannhulol@gmail.com>
2023-09-20 18:03:18 -0700

    1

---
+++ vd1.js
@@ -0,0 +1,6 @@
+        var a = "Hello";
+        var b = "Dhnynit";
+        var c = a + " " + b;
+        alert(c);
+        // document.write(c);
+        document.getElementById("kq").innerText = `<p>@c</p>`;
